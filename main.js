64a77c61de4e9a771a9d5534b8a3e6b75b9aac69
spdskatr <tunan.shi@gmail.com>
2018-04-23 14:44:18 +1000

    Content

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-const palette = ["111889", "d7d88d", "17f17a", /*"79c1c2", This is the colour of the background */ "21a704", "d98000"]
+const palette = ["111889", "d7d88d", "17f17a", "79c1c2", "21a704", "d98000", "000000"]
+var flashing = false;
+
+function rave() {
+    if (!flashing) {
+        flashing = true
+        setInterval(changeNumberColor, 200)
+    }
+}
 
 function changeNumberColor() {
-    document.getElementById("thenumber").setAttribute("style", "color: #" + palette[Math.floor(Math.random() * palette.length)])
+    let numbers = document.getElementsByClassName("mynumber")
+    let color = palette[Math.floor(Math.random() * palette.length)];
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].style.color = "#" + color
+    }
 }
