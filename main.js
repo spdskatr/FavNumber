d9aa829420e1ca99b9a36222a109c184fd9de726
spdskatr <tunan.shi@gmail.com>
2018-04-30 22:16:58 +1000

    More facts

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const palette = ["111889", "d7d88d", "17f17a", "79c1c2", "21a704", "d98000", "000000"]
 var flashing = false;
+var four = 0;
+document.addEventListener("keydown", ev => {
+    if (ev.key == '4') {
+        four++;
+        if (four == 8) {
+            window.location.href = "http://battlefordreamisland.wikia.com/wiki/Four"
+        }
+    }
+})
+
 
 function rave() {
     if (!flashing) {
@@ -10,7 +20,7 @@ function rave() {
 
 function changeNumberColor() {
     let numbers = document.getElementsByClassName("mynumber")
-    let color = palette[Math.floor(Math.random() * palette.length)];
+    let color = palette[Math.floor(Math.random() * palette.length)]
     for (let i = 0; i < numbers.length; i++) {
         numbers[i].style.color = "#" + color
     }
