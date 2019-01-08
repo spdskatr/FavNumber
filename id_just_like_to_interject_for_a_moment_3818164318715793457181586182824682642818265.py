5f455e9162badb34d8ac6a4dcd9c602b414120c6
spdskatr <25078279+spdskatr@users.noreply.github.com>
2019-01-08 17:19:31 +1100

    Create id_just_like_to_interject_for_a_moment_3818164318715793457181586182824682642818265.py

---
+++ id_just_like_to_interject_for_a_moment_3818164318715793457181586182824682642818265.py
@@ -0,0 +1,9 @@
+     # Ha, you think you were going to find the algorithm here? 
+                  # Well you thought wrong! 
+      # Implement it yourself, kid
+                                     #
+# #TeamNoelle
+   # btw, look at this INDENTING
+        # ha
+       #ha
+         #    lol. 
