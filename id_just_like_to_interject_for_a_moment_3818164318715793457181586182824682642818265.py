257699022ab210ff755fde97bcb35e665b30a0be
spdskatr <25078279+spdskatr@users.noreply.github.com>
2019-01-14 13:44:01 +1100

    Delete id_just_like_to_interject_for_a_moment_3818164318715793457181586182824682642818265.py

--- id_just_like_to_interject_for_a_moment_3818164318715793457181586182824682642818265.py
+++
@@ -1,9 +0,0 @@
-     # Ha, you think you were going to find the algorithm here? 
-                  # Well you thought wrong! 
-      # Implement it yourself, kid
-                                     #
-# #RIPNoelle
-   # btw, look at this INDENTING
-        # ha
-       #ha
-         #    lol. 
